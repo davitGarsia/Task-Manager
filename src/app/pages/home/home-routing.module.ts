0d9868f013dbb0b41d8d3a159af3559427434d6d
davitGarsia <davitgarsiashviligmail.com>
2023-01-28 20:07:45 +0400

    create home-page branch(contains sidenav and boards)

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home.component";
+import { HomeComponent } from './home.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-  }
+  },
+  {
+    path: 'main',
+    component: MainLayoutComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,15 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { BoardsComponent } from './main-layout/boards/boards.component';
+import { IndividualBoardComponent } from './main-layout/individual-board/individual-board.component';
+import { CreateBoardComponent } from './main-layout/create-board/create-board.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent, MainLayoutComponent, BoardsComponent, IndividualBoardComponent, CreateBoardComponent],
+  imports: [CommonModule, HomeRoutingModule, MatSidenavModule],
 })
-export class HomeModule { }
+export class HomeModule {}
