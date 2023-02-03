c336dab6557c11455dce7c55da9c69671874c84c
davitGarsia <davitgarsiashviligmail.com>
2023-02-03 19:30:14 +0400

    add stepper forms

--- src/app/pages/stepper/add-users/add-users.component.ts
+++ src/app/pages/stepper/add-users/add-users.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-users',
   templateUrl: './add-users.component.html',
-  styleUrls: ['./add-users.component.scss']
+  styleUrls: ['./add-users.component.scss'],
 })
 export class AddUsersComponent implements OnInit {
+  stepperService: any;
+  constructor(private _formBuilder: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  usersFormGroup = this._formBuilder.group({
+    firsName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    idNumber: ['', Validators.required],
+    email: ['', Validators.required],
+    mobNumber: ['', Validators.required],
+  });
+  isEditable = true;
+
+  Submit() {
+    this.stepperService.openNextStep(1);
   }
-
 }
