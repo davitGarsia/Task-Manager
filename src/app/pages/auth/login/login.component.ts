77933bc36bc2c94646207c1d48a3e537cee177df
davitGarsia <davitgarsiashviligmail.com>
2023-02-10 14:01:03 +0400

    add routerlink to stepper

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,40 +1,42 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, UntypedFormGroup, Validators} from "@angular/forms";
-import {AuthService} from "../../../core/services";
-import {Router} from "@angular/router";
-import {Subject, takeUntil} from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../../core/services';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   form: FormGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
   });
-  sub$ = new Subject()
+  sub$ = new Subject();
   validateForm!: UntypedFormGroup;
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-  ) {
-  }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   submit() {
-    this.form.markAllAsTouched()
-    if (this.form.invalid) return
-    this.authService.login(this.form.value)
+    this.form.markAllAsTouched();
+    if (this.form.invalid) return;
+    this.authService
+      .login(this.form.value)
       .pipe(takeUntil(this.sub$))
-      .subscribe(res => {
-        this.router.navigate(['./application']);
-
-      })
+      .subscribe((res) => {
+        this.router.navigate(['/stepper']);
+      });
   }
 }
