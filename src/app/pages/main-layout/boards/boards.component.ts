e7aca0611889ffcb94b9c7bd401c9e19edac7eb5
davitGarsia <davitgarsiashviligmail.com>
2023-02-23 13:18:38 +0400

    basic sidenav added

--- src/app/pages/main-layout/boards/boards.component.ts
+++ src/app/pages/main-layout/boards/boards.component.ts
@@ -1,13 +1,14 @@
-import {Component, ViewChild} from '@angular/core';
-import {MatDrawer} from "@angular/material/sidenav";
-import {DrawerService} from "../../../core/services/drawer.service";
+import { Component, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { DrawerService } from '../../../core/services/drawer.service';
 
 @Component({
   selector: 'app-boards',
   templateUrl: './boards.component.html',
-  styleUrls: ['./boards.component.scss']
+  styleUrls: ['./boards.component.scss'],
 })
 export class BoardsComponent {
+  isOpen = false;
   @ViewChild(MatDrawer) drawer!: MatDrawer;
 
   constructor(private drawerService: DrawerService) {}
@@ -16,14 +17,22 @@ export class BoardsComponent {
     console.log('hi');
     this.drawerService.isDrawerOpen$.subscribe((res: boolean) => {
       if (res) {
-        this.drawer.open();
+        this.drawer?.open();
+        // this.isOpen = true;
       } else {
         this.drawer?.close();
+        // this.isOpen = false;
       }
     });
   }
 
-  closeDrawer() {
-    this.drawerService.closeDrawer();
+  toggleDrawer() {
+    if (!this.isOpen) {
+      this.drawerService.openDrawer();
+      this.isOpen = true;
+    } else if (this.isOpen) {
+      this.drawerService.closeDrawer();
+      this.isOpen = false;
+    }
   }
 }
